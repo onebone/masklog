0d2c6940ac7958bc0dda73f40f858ef91867affe
onebone <jyc00410@gmail.com>
2020-03-12 03:40:22 +0900

    Add constant of stock info status

--- src/main/kotlin/data.kt
+++ src/main/kotlin/data.kt
@@ -17,6 +17,15 @@ const val STORE_TYPE_PHARMACY = "01"
 const val STORE_TYPE_POST_OFFICE = "02"
 const val STORE_TYPE_NONGHYUP = "03"
 
+// (green) 100 <= x
+const val REMAIN_PLENTY = "plenty"
+// (yellow) 30 <= x < 100
+const val REMAIN_SOME = "some"
+// (red) 2 <= x < 30
+const val REMAIN_FEW = "few"
+// (gray) 0 <= x < 2
+const val REMAIN_EMPTY = "empty"
+
 // https://app.swaggerhub.com/apis-docs/Promptech/public-mask-info/20200307-oas3#/Store
 data class Store (
 	val code: String,
